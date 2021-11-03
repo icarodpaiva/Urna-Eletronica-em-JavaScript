1f8c13bbd5cf47b3f009f7fd922e5f0f81e83a90
icaro paiva <icaropaivadbz.gmail.com>
2021-11-03 14:29:11 -0300

    adicionado candidatos no arquivo etapas

---
+++ etapas.js
@@ -0,0 +1,46 @@
+let etapas = [
+  {
+    titulo: 'VEREADOR',
+    numeros: 5,
+    candidatos: [
+      {
+        numero: 12103,
+        nome: 'Binladen',
+        partido: 'PDT',
+        fotos: [{ url: 'binladen.jpg', legenda: 'Vereador', small: false }]
+      },
+      {
+        numero: 10100,
+        nome: 'Morto',
+        partido: 'PRB',
+        fotos: [{ url: 'morto.jpg', legenda: 'Vereador', small: false }]
+      }
+    ]
+  },
+  {
+    titulo: 'PREFEITO',
+    numeros: 2,
+    candidatos: [
+      {
+        numero: 51,
+        nome: 'Mano',
+        partido: 'ABC',
+        vice: 'Clark Crente',
+        fotos: [
+          { url: 'mano.jpg', legenda: 'Prefeito', small: false },
+          { url: 'clark.jpg', legenda: 'Vice-Prefeito', small: true }
+        ]
+      },
+      {
+        numero: 66,
+        nome: 'Clark Crente',
+        partido: 'PDT',
+        vice: 'Mano',
+        fotos: [
+          { url: 'clark.jpg', legenda: 'Prefeito', small: false },
+          { url: 'mano.jpg', legenda: 'Vice-Prefeito', small: true }
+        ]
+      }
+    ]
+  }
+]
